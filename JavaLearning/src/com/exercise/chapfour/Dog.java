8ec271f61384dbafed944dd794d831f8782c04dc
ipritish <pritishjain2009@gmail.com>
2015-08-20 20:54:12 +0530

    chap four

---
+++ JavaLearning/src/com/exercise/chapfour/Dog.java
@@ -0,0 +1,24 @@
+package com.exercise.chapfour;
+
+public class Dog 
+{
+	int size;
+	String name;
+	
+	void bark()
+	{
+		if(size > 60)
+		{
+			System.out.println("Woof! Woof!");
+		}
+		else if (size > 14)
+		{
+			System.out.println("Ruff! Ruff!");
+		}
+		else
+		{
+			System.out.println("Yip! Yip!");
+		}
+	}
+
+}
