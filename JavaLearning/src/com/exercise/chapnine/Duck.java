a398e3c117d317f25ec196e1cf8a30daefc60815
ipritish <pritishjain2009@gmail.com>
2015-08-27 21:02:42 +0530

    chap nine

---
+++ JavaLearning/src/com/exercise/chapnine/Duck.java
@@ -0,0 +1,13 @@
+package com.exercise.chapnine;
+
+public class Duck {
+	
+	int size;
+	
+	public Duck(int duckSize)
+	{
+		size = duckSize;
+		System.out.println("size is: " + size);
+	}
+
+}
---
+++ JavaLearning/src/com/exercise/chapnine/UseADuck.java
@@ -0,0 +1,10 @@
+package com.exercise.chapnine;
+
+public class UseADuck {
+
+	public static void main(String[] args) {
+		Duck duck = new Duck(45);
+
+	}
+
+}
