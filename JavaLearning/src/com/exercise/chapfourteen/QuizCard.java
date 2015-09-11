8036aec9d7322e5fa4e93ec280f5081e8261ebce
ipritish <pritishjain2009@gmail.com>
2015-09-11 20:36:25 +0530

    quiz card

---
+++ JavaLearning/src/com/exercise/chapfourteen/QuizCard.java
@@ -0,0 +1,22 @@
+package com.exercise.chapfourteen;
+
+public class QuizCard {
+
+	private String question;
+	private String answer;
+	
+	public QuizCard(String q, String a)
+	{
+		question = q;
+		answer = a;
+	}
+	public String getQuestion()
+	{
+		return question;
+	}
+	
+	public String getAnswer()
+	{
+		return answer;
+	}
+}
---
+++ JavaLearning/src/com/exercise/chapfourteen/QuizCardBuilder.java
@@ -0,0 +1,10 @@
+package com.exercise.chapfourteen;
+
+public class QuizCardBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
