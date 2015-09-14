a702907e719a28284c441f8a460603cf0191e92a
ipritish <pritishjain2009@gmail.com>
2015-09-14 20:55:41 +0530

    trial

--- JavaLearning/src/com/exercise/chapfourteen/QuizCardBuilder.java
+++ JavaLearning/src/com/exercise/chapfourteen/QuizCardBuilder.java
@@ -1,10 +1,16 @@
 package com.exercise.chapfourteen;
 
+import java.util.ArrayList;
+
+import javax.swing.JTextArea;
+
 public class QuizCardBuilder {
 
+	private JTextArea question;
+	private JTextArea answer;
+	private ArrayList<QuizCard> cardList;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
