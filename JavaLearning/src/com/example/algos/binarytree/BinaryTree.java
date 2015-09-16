bfb081b4692e129d2e0702a9fdfec3d65cbac743
ipritish <pritishjain2009@gmail.com>
2015-09-16 21:19:37 +0530

    tree node

---
+++ JavaLearning/src/com/example/algos/binarytree/BinaryTree.java
@@ -0,0 +1,43 @@
+package com.example.algos.binarytree;
+
+public class BinaryTree {
+
+	TreeNode root;
+	
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.startInterface();
+	}
+	
+	public void startInterface()
+	{
+		root = createNode("Root Node");
+		TreeNode firstLeft = createNode("First Left");
+		root.leftChild = firstLeft;
+		TreeNode firstRight = createNode("First Right");
+		root.rightChild = firstRight;
+		traverseTree(root);
+	}
+	
+	public TreeNode createNode(String nodeValue){
+		TreeNode newNode = new TreeNode();
+		newNode.nodeName = nodeValue;
+		newNode.leftChild = null;
+		newNode.rightChild = null;
+		return newNode;
+	}
+	
+	public void traverseTree(TreeNode root)
+	{
+		if(root != null)
+		{
+			System.out.println(root.nodeName);
+			if(root.leftChild != null)
+				traverseTree(root.leftChild);
+			else if(root.rightChild != null)
+				traverseTree(root.leftChild);
+		}
+		
+	}
+
+}
