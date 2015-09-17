8b7ded88de92fb6b0ef14df907b4a28e489f3ddf
ipritish <pritishjain2009@gmail.com>
2015-09-17 19:18:09 +0530

    chap fifteen start

---
+++ JavaLearning/src/com/exercise/chapfifteen/DailyAdviceClient.java
@@ -0,0 +1,10 @@
+package com.exercise.chapfifteen;
+
+public class DailyAdviceClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaLearning/src/com/exercise/chapfifteen/DailyAdviceServer.java
@@ -0,0 +1,10 @@
+package com.exercise.chapfifteen;
+
+public class DailyAdviceServer {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
