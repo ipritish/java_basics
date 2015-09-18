e1b8f8c2add2a629fff478d9b6d18a881293cea1
ipritish <pritishjain2009@gmail.com>
2015-09-18 19:02:52 +0530

    advice server

--- JavaLearning/src/com/exercise/chapfifteen/DailyAdviceClient.java
+++ JavaLearning/src/com/exercise/chapfifteen/DailyAdviceClient.java
@@ -1,9 +1,36 @@
 package com.exercise.chapfifteen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class DailyAdviceClient {
 
+	public void go()
+	{
+		try {
+			Socket s = new Socket("127.0.0.1",4242);
+			
+			InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+			BufferedReader reader = new BufferedReader(streamReader);
+			
+			String advice = reader.readLine();
+			System.out.println("Today you should : " + advice);
+			
+			reader.close();
+			s.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DailyAdviceClient client = new DailyAdviceClient();
+		client.go();
 
 	}
 
--- JavaLearning/src/com/exercise/chapfifteen/DailyAdviceServer.java
+++ JavaLearning/src/com/exercise/chapfifteen/DailyAdviceServer.java
@@ -1,9 +1,46 @@
 package com.exercise.chapfifteen;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class DailyAdviceServer {
 	
+	String[] adviceList = {"Take Smaller bites","Go for the tight jeans. NO they do not "
+			+ "make you look fat"," One word: Inappropriate","Just for today be honest",
+			"You might want to rethink that haircut"};
+	
+	public void go(){
+		try {
+			ServerSocket serverSock = new ServerSocket(4242);
+			
+			while(true)
+			{
+				Socket sock = serverSock.accept();
+				
+				PrintWriter writer = new PrintWriter(sock.getOutputStream());
+				String advice = getAdvice();
+				writer.println(advice);
+				writer.close();
+				System.out.println(advice);
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String getAdvice()
+	{
+		int random = (int) (Math.random() * adviceList.length);
+		return adviceList[random];
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DailyAdviceServer server = new DailyAdviceServer();
+		server.go();
 
 	}
 
