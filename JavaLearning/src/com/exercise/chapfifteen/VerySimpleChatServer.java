5df6b55de8c36213336cf58188be2ab1fcd91fbb
ipritish <pritishjain2009@gmail.com>
2015-09-20 02:16:45 +0530

    chat client

--- JavaLearning/src/com/exercise/chapfifteen/VerySimpleChatServer.java
+++ JavaLearning/src/com/exercise/chapfifteen/VerySimpleChatServer.java
@@ -1,6 +1,25 @@
 package com.exercise.chapfifteen;
 
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class VerySimpleChatServer {
+	
+	@SuppressWarnings("rawtypes")
+	ArrayList clientOutputStreams;
+	
+	public class ClientHandler implements Runnable{
+
+		@Override
+		public void run() {
+			BufferedReader reader;
+			Socket sock;
+			
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
