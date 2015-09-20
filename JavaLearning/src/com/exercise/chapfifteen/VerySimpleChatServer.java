e0c0e8e65c9d5e80d37029143e1e1ff528072725
ipritish <pritishjain2009@gmail.com>
2015-09-20 22:01:37 +0530

    chat app

--- JavaLearning/src/com/exercise/chapfifteen/VerySimpleChatServer.java
+++ JavaLearning/src/com/exercise/chapfifteen/VerySimpleChatServer.java
@@ -1,20 +1,52 @@
 package com.exercise.chapfifteen;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class VerySimpleChatServer {
 	
-	@SuppressWarnings("rawtypes")
-	ArrayList clientOutputStreams;
+	
+	ArrayList<PrintWriter> clientOutputStreams;
 	
 	public class ClientHandler implements Runnable{
-
+		
+		BufferedReader reader;
+		Socket sock;
+		
+		public ClientHandler(Socket clientSocket) {
+			try
+			{
+				sock = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(isReader);
+				
+				
+			}
+			catch (Exception ie)
+			{
+				ie.printStackTrace();
+			}
+			
+		}
+		
 		@Override
 		public void run() {
-			BufferedReader reader;
-			Socket sock;
+			String message;
+			try{
+				while((message = reader.readLine()) != null)
+				{
+					tellEveryOne(message);
+				}
+					
+			}
+			catch(Exception ie){
+				ie.printStackTrace();
+			}
 			
 			
 		}
@@ -22,8 +54,45 @@ public class VerySimpleChatServer {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new VerySimpleChatServer().go();
 
+	}
+	
+	public void go(){
+		clientOutputStreams = new ArrayList<PrintWriter>();
+		try{
+			ServerSocket serverSock = new ServerSocket(5000);
+			while(true)
+			{
+				Socket clientSocket = serverSock.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputStreams.add(writer);
+				
+				Thread t = new Thread(new ClientHandler(clientSocket));
+				t.start();
+				System.out.println("got a connection");
+			}
+		}
+		catch(Exception ie){
+			ie.printStackTrace();
+		}
+	}
+	
+	public void tellEveryOne(String message)
+	{
+		Iterator<PrintWriter> it = clientOutputStreams.iterator();
+		while(it.hasNext())
+		{
+			try{
+				PrintWriter writer = (PrintWriter) it.next();
+				writer.println(message);
+				writer.flush();
+			}
+			catch(Exception ie)
+			{
+				ie.printStackTrace();
+			}
+		}
 	}
 
 }
